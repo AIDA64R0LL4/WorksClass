fd2c29d47b690d727197917cb9ccfe17353bf440
Артём <124433822+AIDA64R0LL4@users.noreply.github.com>
2023-05-25 21:49:40 +0500

    Add files via upload

---
+++ CatClass/Zadanie1/Cat.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie1
+{
+    internal class Cat
+    {
+        private string name;
+        private double weight;
+
+        public Cat(string CatName)
+        {
+            Name = CatName;
+        }
+        public void Meow()
+        {
+            Console.WriteLine($"{name}: МЯУ!!!");
+        }
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                bool OnlyLetters = true;
+                foreach (var ch in value)
+                {
+                    if (!char.IsLetter(ch))
+                    {
+                        OnlyLetters = false;
+                    }
+                }
+                if (OnlyLetters)
+                {
+                    name = value;
+                }
+                else
+                {
+                    Console.WriteLine($"{value} - неправальное имя для кота!");
+                }
+            }
+        }
+        public double Weight
+        {
+            get
+            {
+                return weight;
+            }
+            set
+            {
+                if(value !=0)
+                {
+                    weight = value;
+                }
+            }
+        }
+        public void AddWeight(double newweight)
+        {
+            if (newweight >= 0)
+            {
+                weight = newweight;
+            }
+            else
+            {
+                Console.WriteLine($"Вес кота не может быть отрицательным!");
+            }
+        }
+        public void Weights()
+        {
+            Console.WriteLine($"Вес {name} : {weight}кг");
+        }
+    }
+}
---
+++ CatClass/Zadanie1/Program.cs
@@ -0,0 +1,34 @@
+﻿using Zadanie1;
+
+class Zadanide1
+{
+    static void Main(string[] args)
+    {
+        Cat murzik = new Cat("Мурзик");
+        Cat batkis = new Cat("Барсик");
+        Console.WriteLine("================");
+        murzik.Name = "Мурзик"; //правильное имя
+        murzik.Meow();
+        murzik.Name = "1234";  // неправильное имя
+        Console.WriteLine("================");
+        murzik.Name = "Мурзикa";
+        murzik.AddWeight(-2); //неправильный вес
+        murzik.Weights();
+        Console.WriteLine("================");
+        murzik.AddWeight(5); //правильный вес
+        murzik.Weights();
+        Console.WriteLine("================");
+        batkis.Name = "Барсик"; //правильное имя
+        batkis.Meow();
+        batkis.Name = "1234";  // неправильное имя
+        Console.WriteLine("================");
+        batkis.Name = "Барсика";
+        batkis.AddWeight(-2); //неправильный вес
+        batkis.Weights();
+        Console.WriteLine("================");
+        Console.WriteLine("================");
+        batkis.AddWeight(5); //правильный вес
+        batkis.Weights();
+        Console.WriteLine("================");
+    }
+}
